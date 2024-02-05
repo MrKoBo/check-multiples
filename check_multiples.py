1c94dca3bb531413e118b6366306ce42663e510f
MrKoBo <kdtate4@memphis.edu>
2024-02-05 15:00:53 -0600

    Added random generation of number and divisor

--- check_multiples.py
+++ check_multiples.py
@@ -1,11 +1,11 @@
-
+import random
 def check_multiples(number, divisor):
     if number % divisor == 0:
         print(f"{number} is divisible by {divisor}")
     else:
         print(f"{number} is not divisible by {divisor}")
 
-number = 75
+number = random.randint(1, 100)
 
-divisor = 5
+divisor = random.randint(1, 10)
 check_multiples(number, divisor)
