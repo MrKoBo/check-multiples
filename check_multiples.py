c3b360df7f9885f4b8df5177fe94a157c69ee370
MrKoBo <kdtate4@memphis.edu>
2024-02-05 14:56:48 -0600

    Refactored check multiple to use a function

--- check_multiples.py
+++ check_multiples.py
@@ -1,8 +1,11 @@
+
+def check_multiples(number, divisor):
+    if number % divisor == 0:
+        print(f"{number} is divisible by {divisor}")
+    else:
+        print(f"{number} is not divisible by {divisor}")
+
 number = 75
 
 divisor = 5
-
-if number % divisor == 0:
-    print(f"{number} is divisible by {divisor}")
-else:
-    print(f"{number} is not divisible by {divisor}")
+check_multiples(number, divisor)
